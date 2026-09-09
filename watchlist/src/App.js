e9e370540f5c9ea0515684eb0f72d847597aaccb
Priyavrat Misra <priyavratmisra123@gmail.com>
2026-09-09 08:13:51 +0530

    add search form

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <p>watchlist</p>;
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(query);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search OMDb"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
 
 export default App;
